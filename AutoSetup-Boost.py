892519436e8171148df86c9b24a2713e1ad15273
mangcut553 <Nu9614685@gmail.com>
2026-06-17 17:59:57 +0700

    Create AutoSetup-Boost.py

---
+++ AutoSetup-Boost.py
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd
+if [ -e "/data/data/com.termux/files/home/storage" ]; then
+    rm -rf /data/data/com.termux/files/home/storage
+fi
+termux-setup-storage
+yes | pkg update
+. <(curl https://cdn.quanghuynopro.com/store/termux-change-repo.sh)
+yes | pkg upgrade
+yes | pkg i python
+yes | pkg i python-pip 
+pip install httpx requests colorama
+curl -Ls "https://raw.githubusercontent.com/mangcut553/Install-Exension-Boost-Cloudphone/refs/heads/main/AutoSetup%2CBoost.py" -o /sdcard/Download/AutoSetup-Boost.py
+
+if ! command -v su >/dev/null 2>&1 || ! su -c 'exit' >/dev/null 2>&1; then
+    exit
+fi
+su -c "settings put global development_settings_enabled 1" && su -c "am kill-all" && su -c "cd /sdcard/Download && export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python ./AutoSetup-Boost.py"
