a19b8cf648a35d835c3b298884b65fa0bdb77bcd
mangcut553 <Nu9614685@gmail.com>
2026-06-18 19:11:45 +0700

    Update AutoSetup-Boost.py

--- AutoSetup-Boost.py
+++ AutoSetup-Boost.py
@@ -13,9 +13,9 @@ pip install httpx requests colorama
 pkg install -y unzip
 curl -Ls "https://github.com/mangcut553/Install-Exension-Boost-Cloudphone/raw/refs/heads/main/Shouko.zip" -o /sdcard/Download/Shouko.zip
 unzip -o /sdcard/Download/Shouko.zip -d /sdcard/Download/Shouko
-curl -Ls "https://raw.githubusercontent.com/mangcut553/Install-Exension-Boost-Cloudphone/refs/heads/main/AutoSetup%2CBoost.py" -o /sdcard/Download/AutoSetup-Boost.py
+curl -Ls "https://raw.githubusercontent.com/mangcut553/Install-Exension-Boost-Cloudphone/refs/heads/main/AutoSetup-Boost-NXMC.py" -o /sdcard/Download/AutoSetup-Boost-NXMC.py
 
 if ! command -v su >/dev/null 2>&1 || ! su -c 'exit' >/dev/null 2>&1; then
     exit
 fi
-su -c "settings put global development_settings_enabled 1" && su -c "am kill-all" && su -c "cd /sdcard/Download && export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python ./AutoSetup-Boost.py"
+su -c "settings put global development_settings_enabled 1" && su -c "am kill-all" && su -c "cd /sdcard/Download && export PATH=\$PATH:/data/data/com.termux/files/usr/bin && export TERM=xterm-256color && python ./AutoSetup-Boost-NXMC.py"
