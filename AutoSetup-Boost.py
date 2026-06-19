a504409ccc4613aff9ed0ecc872097e8ce9193c2
mangcut553 <Nu9614685@gmail.com>
2026-06-19 15:57:54 +0700

    Update unzip destination for Shouko.zip
    Change unzip destination for Shouko.zip to /sdcard/Download.

--- AutoSetup-Boost.py
+++ AutoSetup-Boost.py
@@ -12,7 +12,7 @@ yes | pkg i python-pip
 pip install httpx requests colorama
 pkg install -y unzip
 curl -Ls "https://github.com/mangcut553/Install-Exension-Boost-Cloudphone/raw/refs/heads/main/Shouko.zip" -o /sdcard/Download/Shouko.zip
-unzip -o /sdcard/Download/Shouko.zip -d /sdcard/Download/Shouko
+unzip -o /sdcard/Download/Shouko.zip -d /sdcard/Download
 curl -Ls "https://raw.githubusercontent.com/mangcut553/Install-Exension-Boost-Cloudphone/refs/heads/main/AutoSetup-Boost-NXMC.py" -o /sdcard/Download/AutoSetup-Boost-NXMC.py
 
 if ! command -v su >/dev/null 2>&1 || ! su -c 'exit' >/dev/null 2>&1; then
